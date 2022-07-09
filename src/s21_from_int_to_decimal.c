1e899f0d39917eacce670ef10832d1903f2c7226
Antoinette Barrett <antoinet@oa-b6.msk.21-school.ru>
2022-07-09 20:08:34 +0300

    from int to decimal done

--- src/s21_from_int_to_decimal.c
+++ src/s21_from_int_to_decimal.c
@@ -1,5 +1,8 @@
 #include "s21_decimal.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-  return dst->bits[0] = src;
+  if (!dst) return ERROR_CONVERT;  //  if memory wasn`t located
+  memset(dst, 0, sizeof(int) * 4);  //  fill the structure with 0s
+  if (src < 0) fill_negative(dst);  //  need this function - set 32th bit in bits[3] to 1;
+  return dst->bits[0] = abs(src);  //  return abs because negative sign keeps separeted
 }
